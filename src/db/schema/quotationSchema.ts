9461766619770219e63c8627edffcd881811a25e
Fernando <fernando.cortes@ewally.com.br>
2023-10-21 12:40:22 -0300

    TS

---
+++ src/db/schema/quotationSchema.ts
@@ -0,0 +1,14 @@
+import { CreateTableInput } from 'test-utils'
+
+export const quotationSchema: CreateTableInput = {
+  TableName: String(process.env.QUOTATION_TABLE),
+  KeySchema: [
+    { AttributeName: 'accountName', KeyType: 'HASH' },
+    { AttributeName: 'id', KeyType: 'RANGE' }
+  ],
+  AttributeDefinitions: [
+    { AttributeName: 'accountName', AttributeType: 'S' },
+    { AttributeName: 'id', AttributeType: 'S' }
+  ],
+  BillingMode: 'PAY_PER_REQUEST'
+}
