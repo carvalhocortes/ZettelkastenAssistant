60c8d2881328c6073817b3f84a3df3a6e044498f
Fernando <fernando.cortes@ewally.com.br>
2023-11-02 17:52:11 -0300

    refactor

--- src/util/validatorUtil.js
+++ src/util/validatorUtil.js
@@ -1,3 +1,9 @@
-module.exports.requiredAndType = (field) => {
+const errors = require ('../common/errorMessages')
 
+const checkRequired = (obj, fieldName) => {
+  if (obj === undefined || obj === null || obj === '' || obj === `:${fieldName}`) throw errors.requiredField(fieldName)
+}
+
+module.exports = {
+  checkRequired
 }
--- src/validator/userValidator.js
+++ src/validator/userValidator.js
@@ -1,33 +1,35 @@
 const errors = require ('../common/errorMessages')
+const { checkRequired } = require('../util/validatorUtil')
 
 const validateLogin = event => {
   if (!event.body) throw errors.requiredField('body')
-  isRequired(event.body.email, 'email')
-  isRequired(event.body.password, 'password')
+  checkRequired(event.body.email, 'email')
+  checkRequired(event.body.password, 'password')
   return event.body
 }
 
 const validateCreateUser = event => {
   if (!event.body) throw errors.requiredField('body')
-  isRequired(event.body.email, 'email')
-  isRequired(event.body.password, 'password')
-  isRequired(event.body.city, 'city')
-  isRequired(event.body.country, 'country')
+  checkRequired(event.body.email, 'email')
+  checkRequired(event.body.password, 'password')
+  checkRequired(event.body.city, 'city')
+  checkRequired(event.body.country, 'country')
   return event.body
 }
 
 const validateGetUser = (event) => {
   if (!event.pathParameters) throw errors.requiredField('pathParameters')
-  isRequired(event.pathParameters.email, 'email')
+  checkRequired(event.pathParameters.email, 'email')
   return event.pathParameters
 }
 
 const validateUpdateUser = (event) => {
   if (!event.pathParameters) throw errors.requiredField('pathParameters')
-  isRequired(event.pathParameters.email, 'email')
+  checkRequired(event.pathParameters.email, 'email')
   if (!event.body) throw errors.requiredField('body')
   const body = event.body
   if (body.email) throw errors.invalidUpdateField('email')
+  if (body.permission) throw errors.invalidUpdateField('permission')
   if (body.status) throw errors.invalidUpdateField('status')
   if (Object.keys(body).length < 1) throw errors.requiredField('body')
   return event
@@ -35,28 +37,25 @@ const validateUpdateUser = (event) => {
 
 const validateDeleteUser = (event) => {
   if (!event.pathParameters) throw errors.requiredField('pathParameters')
-  isRequired(event.pathParameters.email, 'email')
+  checkRequired(event.pathParameters.email, 'email')
   return event.pathParameters
 }
 
 const validateActivateUser = (event) => {
   if (!event.pathParameters) throw errors.requiredField('pathParameters')
-  isRequired(event.pathParameters.token, 'token')
+  checkRequired(event.pathParameters.token, 'token')
   return event.pathParameters
 }
 
 const validateUnlockUser = (event) => {
   if (!event.body) throw errors.requiredField('body')
-  isRequired(event.body.token, 'token')
-  isRequired(event.body.password, 'password')
+  checkRequired(event.body.token, 'token')
+  checkRequired(event.body.password, 'password')
   return event.body
 }
 
 // PRIVATE FUNCTIONS
 
-const isRequired = (obj, fieldName) => {
-  if (obj === undefined || obj === null || obj === '' || obj === `:${fieldName}`) throw errors.requiredField(fieldName)
-}
 
 module.exports = {
   validateLogin,
