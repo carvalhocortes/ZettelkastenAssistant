91c779da353a47a1b96e470cd0ba45a6618eab61
Fernando <fernando.cortes@ewally.com.br>
2023-11-06 12:24:16 -0300

    adds getFileData, askToFile

--- src/connector/docalysisConnector.js
+++ src/connector/docalysisConnector.js
@@ -7,13 +7,24 @@ const fileErrors = require('../common/fileErrors')
 const baseUrl = 'https://api1.docalysis.com/api/v1'
 
 const sentFileToDocalysis = async (fileName, url, token) => {
-  //pegar o token
   const headers = assembleAuthHeader(token)
   const file = {
     name: fileName,
     url
   }
   const response = await docalysisApi.post('/files/create', file, { headers })
+  return checkDocalysisResponseSuccess(response)
+}
+
+const getFileData = async (fileId, token) => {
+  const headers = assembleAuthHeader(token)
+  const response = await docalysisApi.get(`/files/${fileId}/info`, { headers })
+  return checkDocalysisResponseSuccess(response)
+}
+
+const askToFile = async (fileId, data, token) => {
+  const headers = assembleAuthHeader(token)
+  const response = await docalysisApi.get(`/files/${fileId}/chat`, { headers, data })
   return checkDocalysisResponseSuccess(response)
 }
 
