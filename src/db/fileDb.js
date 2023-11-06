6b52aa819bfd9a4d457bff6482a196f382c37fd3
Fernando <fernando.cortes@ewally.com.br>
2023-11-06 10:27:17 -0300

    adds getter method

--- src/db/fileDb.js
+++ src/db/fileDb.js
@@ -1,16 +1,24 @@
+const uuid = require('uuid').v4
+
 const dynamoDbUtil = require('../util/dynamoDbUtil')
 const constants = require('../common/constants')
 
-const tableName = process.env.TABLE_TABLE
+const tableName = process.env.FILES_TABLE
 
-const save = async (user) => dynamoDbUtil.save(user, tableName)
+const save = async (data) => {
+  data.id = uuid()
+  return dynamoDbUtil.save(data, tableName)
+}
 
 const update = async (updateData, id, keysToDelete) => {
   if (updateData.id) delete updateData.id
   return dynamoDbUtil.update(updateData, tableName, { id }, keysToDelete)
 }
 
+const getById = async (id) => dynamoDbUtil.getByKey({ id }, tableName)
+
 module.exports = {
   save,
+  getById,
   update
 }
