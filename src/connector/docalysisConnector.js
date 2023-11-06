d3bf92cb421f3994f599c1e989c1abc08147b6f5
Fernando <fernando.cortes@ewally.com.br>
2023-11-06 10:28:25 -0300

    feat createApi

--- src/connector/docalysisConnector.js
+++ src/connector/docalysisConnector.js
@@ -1,9 +1,50 @@
-const sentFileToDocalysis = async (fileName, url, user) => {
+'use strict'
 
-  return { data: 'data'}
+const axios = require('axios')
+const { log } = require('../util/loggerUtil')
+const fileErrors = require('../common/fileErrors')
+
+const baseUrl = 'https://api1.docalysis.com/api/v1'
+
+const sentFileToDocalysis = async (fileName, url, token) => {
+  //pegar o token
+  const headers = assembleAuthHeader(token)
+  const file = {
+    name: fileName,
+    url
+  }
+  const response = await docalysisApi.post('/files/create', file, { headers })
+  return checkDocalysisResponseSuccess(response)
 }
 
 // PRIVATE FUNCTIONS
+
+const createApi = ({
+  onFulfilled = ({ data }) => data,
+  onRejected = err => err
+} = {}) => {
+  const api = axios.create({
+    baseURL: encodeURI(baseUrl),
+    timeout: 60000
+  })
+  api.interceptors.response.use(onFulfilled, onRejected)
+  return api
+}
+
+const docalysisApi = createApi({ onRejected: null })
+
+const assembleAuthHeader = (token, headers = {}) => ({
+  ...headers,
+  Authorization: `Bearer ${token}`
+})
+
+const checkDocalysisResponseSuccess = (response) => {
+  if (response?.success !== true) {
+    log({ DocalysisError: response?.error })
+    throw fileErrors.docalysisError
+  }
+  return response
+}
 
 module.exports = {
   sentFileToDocalysis
