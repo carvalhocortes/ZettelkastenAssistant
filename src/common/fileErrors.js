a94b5643864a069b5e30e4590cd57028f2879e33
Fernando <fernando.cortes@ewally.com.br>
2023-11-06 10:12:41 -0300

    adds errors to connectors

--- src/common/fileErrors.js
+++ src/common/fileErrors.js
@@ -11,4 +11,14 @@ module.exports = {
     code: errorCode(2),
     msg: `The requested command is not valid.`
   },
+  docalysisError: {
+    httpCode: 400,
+    code: errorCode(3),
+    msg: 'Unknown error on Docalysis'
+  },
+  mendeleyError: {
+    httpCode: 400,
+    code: errorCode(4),
+    msg: 'Unknown error on Mendeley'
+  },
 }
