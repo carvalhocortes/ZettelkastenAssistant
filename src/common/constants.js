6c6aa1fd3887ac096b353a38af22ab479106f360
Fernando <fernando.cortes@ewally.com.br>
2023-11-06 10:28:40 -0300

    others

--- src/common/constants.js
+++ src/common/constants.js
@@ -25,11 +25,40 @@ module.exports = {
     'docx',
     'txt'
   ],
+  processAfter: {
+    docalysis: 'processDocalysis',
+    mendeley: 'processMendeley'
+  },
   file: {
     defaultSignedUrlExpirationInSeconds: 60 * 60,
     status: {
       pending: 'pending',
-      analyzed: 'analyzed'
+      analyzed: 'analyzed',
+      error: 'error'
+    },
+    mendeley: {
+      types: {
+        journal: 'journal',
+        book: 'book',
+        generic: 'generic',
+        book_section: 'book_section',
+        conference_proceedings: 'conference_proceedings',
+        working_paper: 'working_paper',
+        report: 'report',
+        web_page: 'web_page',
+        thesis: 'thesis',
+        magazine_article: 'magazine_article',
+        statute: 'statute',
+        patent: 'patent',
+        newspaper_article: 'newspaper_article',
+        computer_program: 'computer_program',
+        hearing: 'hearing',
+        television_broadcast: 'television_broadcast',
+        encyclopedia_article: 'encyclopedia_article',
+        case: 'case',
+        film: 'film',
+        bill: 'bill'
+      }
     }
   }
 }
