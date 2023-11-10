8c9f6282972a2162dcef3458ffcd27fc8c6c5997
Fernando <fernando.cortes@ewally.com.br>
2023-11-10 18:10:28 -0300

    adds mendeley tests

--- test/files/handleFileUploadedTest.js
+++ test/files/handleFileUploadedTest.js
@@ -1,24 +1,37 @@
 const fileDb = require('../../src/db/fileDb')
-const {  testSuccess, testError, mockSentFileToDocalysis } = require('../helper/testHelper')
+const userDb = require('../../src/db/userDb')
+const { now } = require('../../src/helper/timeHelper')
+
+const {  testSuccess, testError, mockSentFileToDocalysis, mockCreateMendeleyDocument } = require('../helper/testHelper')
 
 const handleFileUploadedFunc = require('../../src/lambda/files').handleFileUploaded
 
 describe('Handle uploaded file tests', () => {
-  it('Should return error if any problem in Docalysis', async() => {
-    const event = buildS3Event()
-    mockSentFileToDocalysis(assembleDocalysisResponse(false))
-    await testError(handleFileUploadedFunc, event, 400, errorsNumber.docalysisError)
+  before(async () => {
+    const user = await userDb.save({
+      email: 'user@gmail.com',
+      config: {
+        useMendeley: true
+      },
+      mendeleyTokens: {
+        accessToken: now(),
+        validUntil: now() + (30 * 60 * 1000),
+      }
+    })
   })
-  // it('Should return error if any problem in Mendeley', async() => {
-  //   const event = buildS3Event()
-  //   const docalysisResponse = assembleDocalysisResponse(true)
-  //   mockSentFileToDocalysis(docalysisResponse)
-  //   await testError(handleFileUploadedFunc, event, 400, errorsNumber.mendeleyError)
-  // })
+  it('Should return error if any problem in Docalysis', async() => {
+    mockSentFileToDocalysis(assembleDocalysisResponse(false))
+    await testError(handleFileUploadedFunc, buildS3Event(), 400, errorsNumber.docalysisError)
+  })
+  it('Should return error if any problem in Mendeley', async() => {
+    mockSentFileToDocalysis(assembleDocalysisResponse(true))
+    mockCreateMendeleyDocument(assembleCreateMendeleyDocumentResponse(), 404)
+    await testError(handleFileUploadedFunc, buildS3Event(), 400, errorsNumber.mendeleyError)
+  })
   it('Should save file and request analysis', async() => {
     const event = buildS3Event()
     mockSentFileToDocalysis(assembleDocalysisResponse(true))
-    // mockar mendeley
+    mockCreateMendeleyDocument(assembleCreateMendeleyDocumentResponse())
     const { id } = await testSuccess(handleFileUploadedFunc, event)
     const fileData = await fileDb.getById(id)
     fileData.should.have.property('fileName').which.is.equal('fileName date.txt')
@@ -30,7 +43,7 @@ describe('Handle uploaded file tests', () => {
     fileData.should.have.property('docalysisData')
     fileData.docalysisData.should.have.property('id').which.is.equal('2462456')
     fileData.docalysisData.should.have.property('processed_state').which.is.equal('unprocessed')
-    // fileData.should.have.property('mendeleyData')
+    fileData.should.have.property('mendeleyData')
   })
 })
 
@@ -58,10 +71,10 @@ const assembleDocalysisResponse = (success) => {
       id: '2462456',
       created_at: 1686887578000,
       file_size: 184292,
-      file_type: "pdf",
-      name: "my_file.pdf",
+      file_type: 'pdf',
+      name: 'my_file.pdf',
       page_count: 9,
-      processed_state: "unprocessed"
+      processed_state: 'unprocessed'
     }
   }
   return {
@@ -70,7 +83,27 @@ const assembleDocalysisResponse = (success) => {
   }
 }
 
+const assembleCreateMendeleyDocumentResponse = () => ({
+  title: 'Title of the document chosen by Mendeley',
+  type: 'mendeley type',
+  authors: [
+      {
+          first_name: 'William',
+          last_name: 'Baude'
+      }
+  ],
+  websites: [
+      'http://www.website.com'
+  ],
+  id: 'random id',
+  created: '2023-11-10T16:30:47.335Z',
+  file_attached: true,
+  profile_id: 'random profile id',
+  last_modified: '2023-11-10T16:30:47.335Z',
+  abstract: 'a small abstract created by mendeley'
+})
+
 const errorsNumber = {
   docalysisError: 3003,
-  mendeleyError: 3004,
+  mendeleyError: 3006,
 }
