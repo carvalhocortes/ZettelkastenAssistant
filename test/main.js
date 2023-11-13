1a95c43d965222289937bfec4df1361c34d57c8a
Fernando <fernando.cortes@ewally.com.br>
2023-11-13 08:42:22 -0300

    fix tests paths

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 const should = require('should')
 const awsMock = require( 'aws-sdk-mock' )
-awsMock.mock('S3', 'getObject', { Metadata: { type: 'book' } })
+awsMock.mock('S3', 'getObject', { Body: 'I dont know what this content would be like', Metadata: { type: 'book' } })
 
 process.env.SHOW_LOGS = false
 process.env.REGION = 'us-east-2'
@@ -9,12 +9,13 @@ process.env.JWT_SECRET = 'AReallyGoodSecretToJWT'
 process.env.DOCALYSIS_TOKEN = 'us-east-2'
 process.env.MENDELEY_ID = '123PIN'
 process.env.MENDELEY_SECRET = '456PIN'
-process.env.MENDELEY_REDIRECT = ''
+process.env.MENDELEY_REDIRECT = 'redirectURL'
 process.env.MENDELEY_STATE = ''
 process.env.MENDELEY_OAUTH_URL = 'https://api.mendeley.com/oauth/authorize'
 process.env.FILES_TABLE = `zettelkasten-files-${process.env.STAGE}`
 process.env.USERS_TABLE = `zettelkasten-users-${process.env.STAGE}`
 process.env.FILES_BUCKET = `zettelkasten-files-${process.env.STAGE}`
+
 
 process.env.DB_ENDPOINT = 'http://localhost:8000'
 
@@ -29,5 +30,6 @@ describe('Zettelkasten Assistant tests', () => {
   require('./users/deleteTest')
   require('./files/createPreSignedUrlTest')
   require('./files/handleFileUploadedTest')
-  require('./files/scheduledFileProcessTest')
+  require('./scheduledProcess/scheduledFileProcessTest')
+  require('./mendeley/oAuthTests')
 })
